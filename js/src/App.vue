9ab78a46df71e12168b167c21a7010b76ca5628e
kyu-suke <ykaneoka7@gmail.com>
2020-03-28 00:39:19 +0900

    http🍥🍥

--- js/src/App.vue
+++ js/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Http />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Http from './components/Http.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Http
   }
 }
 </script>
---
+++ js/src/components/Http.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>http</h1>
+    <input type="text">
+    <input type="button" value="Add" @click="post()">
+    <ul>
+      <li v-for="t in tasks" :key="t"><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">{{t}}</a></li>
+    </ul>
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Http',
+  data() {
+    return {
+      tasks: ["babel1","eslint2","babel3","eslint4"]
+    }
+  },
+  methods: {
+    post() {
+      axios.get('http://localhost:8080/').then((r) => {
+        console.log(r)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
